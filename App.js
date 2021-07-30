40fdc458e219937b116b8eb4a41febe24898f193
Agus <agustinmatias9@gmail.com>
2021-07-30 01:45:42 -0300

    Hago que se randomize

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
-const button = document.getElementById("btn");
-button.addEventListener("click", () => Motor())
+const buttonStr = document.getElementById("btnStart");
+const btnRandom = document.getElementById("btnRandom");
+const btnRandomVD = document.getElementById("btnRandomVD");
+buttonStr.addEventListener("click", () => Motor())
+btnRandom.addEventListener("click", () => Randomizador())
+btnRandomVD.addEventListener("click", () => RandomizadorVD())
 
 function Motor()
 {
@@ -7,7 +11,6 @@ function Motor()
     var actualizador = Number(document.getElementById("Actualizador").value);
     var cir1 = new Circulo(document.getElementById("Cir1").value,document.getElementById("PosX1").value,document.getElementById("PosY1").value,document.getElementById("VdX1").value,document.getElementById("VdY1").value);
     var cir2 = new Circulo(document.getElementById("Cir2").value,document.getElementById("PosX2").value,document.getElementById("PosY2").value,document.getElementById("VdX2").value,document.getElementById("VdY2").value);
-    
     var canvas = document.getElementById("myCanvas");
     this.Dibujador(cir1,cir2,canvas);
     console.log(cir1);
@@ -45,7 +48,6 @@ function Limpieza(cir,canvas)
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     ctx.clearRect((cir.x + canvas.width/2) - cir.tamaño-1, (cir.y + canvas.height/2)- cir.tamaño - 1, cir.tamaño*2, cir.tamaño*2);
-
 }
 
 class Circulo
@@ -63,4 +65,23 @@ class Circulo
     {
         return new Circulo(this.tamaño,this.x+this.vdx*r,this.y+this.vdy*r,0,0);
     }
+}
+
+function Randomizador() {
+    var canvas = document.getElementById("myCanvas");
+    var x = canvas.width/2;
+    var y = canvas.height/2;
+    console.log(Math.floor(Math.random()*(20-0)+0));
+
+    document.getElementById("PosX1").value = Math.floor(Math.random()*(x-(x*-1))+(x*-1));
+    document.getElementById("PosY1").value = Math.floor(Math.random()*(y-(y*-1))+(y*-1));
+    document.getElementById("PosX2").value = Math.floor(Math.random()*(x-(x*-1))+(x*-1));
+    document.getElementById("PosY2").value = Math.floor(Math.random()*(y-(y*-1))+(y*-1));
+}
+
+function RandomizadorVD() {
+    document.getElementById("VdX1").value = Math.floor(Math.random()*(50-(-50))+(-50));
+    document.getElementById("VdY1").value = Math.floor(Math.random()*(50-(-50))+(-50));
+    document.getElementById("VdX2").value = Math.floor(Math.random()*(50-(-50))+(-50));
+    document.getElementById("VdY2").value = Math.floor(Math.random()*(50-(-50))+(-50));
 }
